28bf6327ae95abc2950bf170bd9b326753665b5a
mkrasanakis <maniospas@hotmail.com>
2021-08-24 13:40:03 +0300

    synthfeats dataset

--- synthfeats/generator.py
+++ synthfeats/generator.py
@@ -4,25 +4,29 @@ rnd = random.Random(0)
 nodes = 1200
 groups = [list(range(300)), list(range(300, nodes//2)), list(range(nodes//2, nodes))]
 pairs = list()
-features = [[rnd.random()*0.3+(0.5 if i < 300 or i >= nodes/2 else 0),
-             rnd.random()*0.3+(0.5 if i > 300 else 0),
-             rnd.random()*0.3+(0.5 if i < nodes/2 else 0),
+noise = 0.2
+features = [[rnd.random()*noise+(0.5 if i < 300 or i >= nodes/2 else 0),
+             rnd.random()*noise+(0.5 if i > 300 else 0),
+             rnd.random()*noise+(0.5 if i < nodes/2 else 0),
              ]
             +
-            [rnd.random() * 0.2 + (0.5 if i < 300 or i >= nodes / 2 else 0),
-             rnd.random() * 0.2 + (0.5 if i > 300 else 0),
-             rnd.random() * 0.2 + (0.5 if i < nodes / 2 else 0),
+            [rnd.random()*noise * 0.2 + (0.5 if i < 300 or i >= nodes / 2 else 0),
+             rnd.random()*noise * 0.2 + (0.5 if i > 300 else 0),
+             rnd.random()*noise * 0.2 + (0.5 if i < nodes / 2 else 0),
              ]
             +
-            [rnd.random()*0.1+(0.5 if i < 300 or i >= nodes/2 else 0),
-             rnd.random()*0.1+(0.5 if i > 300 else 0),
-             rnd.random()*0.1+(0.5 if i < nodes/2 else 0),
+            [rnd.random()*noise+(0.5 if i < 300 or i >= nodes/2 else 0),
+             rnd.random()*noise+(0.5 if i > 300 else 0),
+             rnd.random()*noise+(0.5 if i < nodes/2 else 0),
              ]for i in range(nodes)]
-for i in range(nodes):
-    for j in range(nodes):
-        prob = 0.1 if (i < 300)==(j < 300) else 0.05
-        if rnd.uniform(0, 1) < prob:
-            pairs.append((i, j))
+
+for group_i in range(3):
+    for group_j in range(3):
+        for i in groups[group_i]:
+            for j in groups[group_j]:
+                prob = 0.15 if group_i==group_j else 0.05
+                if rnd.uniform(0, 1) < prob:
+                    pairs.append((i, j))
 
 with open("pairs.txt", "w") as file:
     for i, j in pairs:
